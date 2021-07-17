abc496fddeb08b55df528a2f9ab906c5d6b3edc2
Tahmin Khaleque <z5260093@ad.unsw.edu.au>
2021-07-17 14:14:44 +1000

    Tahmin - Updated Donate Page with Back Navigation

--- app/src/main/java/com/example/a3605firstscientists/Donate.java
+++ app/src/main/java/com/example/a3605firstscientists/Donate.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
@@ -15,17 +16,13 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTube
 
 public class Donate extends AppCompatActivity {
 
-    private YouTubePlayerView youTubePlayerView;
-    private TextView supportMessage;
-    private Button donate;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
 
-        donate = findViewById(R.id.btn_donate);
+        // Method for handling Donate button
+        Button donate = findViewById(R.id.btn_donate);
         donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,13 +31,25 @@ public class Donate extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        supportMessage = (TextView) findViewById(R.id.tv_supportmsg);
+
+        // Method for handling Back button
+        ImageButton back = findViewById(R.id.btn_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Donate.this, LearnActivity.class);
+                intent.putExtra("From", "Donate");
+                startActivity(intent);
+            }
+        });
+
+        TextView supportMessage = findViewById(R.id.tv_supportmsg);
         supportMessage.setText("We’re committed to working respectfully and in partnership with Australia’s First Nations peoples.\n" +
                 "\n" +
                 "We recognise the importance of maintaining and re-establishing connections to culture, country and kinship to supporting Aboriginal and Torres Strait Islander people’s health and wellbeing.\n" +
                 "\n" +
                 "We design programs with Aboriginal and Torres Strait Islander peoples: always aiming to empower people to pursue their goals, and to be culturally appropriate and accessible.");
-        youTubePlayerView = findViewById(R.id.redcrossvideo);
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.redcrossvideo);
         getLifecycle().addObserver(youTubePlayerView);
 
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
