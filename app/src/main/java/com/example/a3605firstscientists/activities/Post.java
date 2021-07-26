cc3dbb8b839d85169ea342f45d777c7257ffda61
marvinma-code <79128479+marvinma-code@users.noreply.github.com>
2021-07-26 20:21:49 +1000

    Authentication and Login update 1

---
+++ app/src/main/java/com/example/a3605firstscientists/activities/Post.java
@@ -0,0 +1,20 @@
+package com.example.a3605firstscientists.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a3605firstscientists.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+
+
+
+}
